9d139b690dea4d5d61eedb92081a5d31d4109573
Ilya Eliseev <ie@e-ise.com>
2015-01-28 01:23:24 +0300

    eiseIntra: placeholder
    Signed-off-by: Ilya Eliseev <ie@e-ise.com>

--- inc_intra.php
+++ inc_intra.php
@@ -478,6 +478,10 @@ function showTextBox($strName, $strValue, $arrConfig=Array()) {
             ($strAttrib ? " ".$strAttrib : "").
             ($strClass ? " ".$strClass : "").
             ($arrConfig["required"] ? " required=\"required\"" : "").
+            ($arrConfig["placeholder"] 
+                ? ' placeholder="'.htmlspecialchars($arrConfig["placeholder"]).'"'
+                    .' title="'.htmlspecialchars($arrConfig["placeholder"]).'"'
+                : "").
             ($arrConfig["maxlength"] ? " maxlength=\"{$arrConfig["maxlength"]}\"" : "").
        " value=\"".htmlspecialchars($strValue)."\" />\r\n";
     } else {
