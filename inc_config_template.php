c37930f7ecbf9b9657bc476eeba4889e5d9f0189
Ilya Eliseev <ie@e-ise.com>
2015-08-21 13:42:30 +0300

    new inc_config_template.php
    Signed-off-by: Ilya Eliseev <ie@e-ise.com>

--- inc_config_template.php
+++ inc_config_template.php
@@ -3,49 +3,61 @@
 error_reporting(7);
 $stpExtendedLog = false;
 
-define ("eiseIntraRelativePath", "/".ltrim(str_replace(
-    str_replace(DIRECTORY_SEPARATOR, "/", $_SERVER["DOCUMENT_ROOT"])
-    , ""
-    , str_replace(DIRECTORY_SEPARATOR, "/", dirname(__FILE__)))
-    ."/"
-    , "/")
+define ('eiseIntraRelativePath', '/'.ltrim(str_replace(
+    str_replace(DIRECTORY_SEPARATOR, '/', $_SERVER['DOCUMENT_ROOT'])
+    , ''
+    , str_replace(DIRECTORY_SEPARATOR, '/', dirname(__FILE__)))
+    .'/'
+    , '/')
 );
 
-define ("eiseIntraAbsolutePath", dirname(__FILE__).DIRECTORY_SEPARATOR);
-define ("commonStuffRelativePath", dirname(eiseIntraRelativePath)."/");
+define ('eiseIntraAbsolutePath', dirname(__FILE__).DIRECTORY_SEPARATOR);
+define ('eiseIntraLibAbsolutePath', dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR);
 
-define ("eiseIntraJSPath",  eiseIntraRelativePath.'js/');
-define ("eiseIntraCSSPath",  eiseIntraRelativePath.'css/');
-define ("eiseIntraCSSTheme", 'bluewing');
+define ('commonStuffRelativePath', dirname(eiseIntraRelativePath).'/');
+define ('eiseIntraLibRelativePath', eiseIntraRelativePath.'lib/');
 
-define ("commonStuffAbsolutePath", dirname(eiseIntraAbsolutePath).DIRECTORY_SEPARATOR);
+define ('eiseIntraJSPath',  eiseIntraRelativePath.'js/');
+define ('eiseIntraCSSPath',  eiseIntraRelativePath.'css/');
 
-define ("jQueryPath", commonStuffRelativePath."jquery/");
-define ("jQueryUIPath", jQueryPath."jquery-ui-1.11.4.custom/");
+define ('eiseIntraCSSTheme', 'default');
 
-define ("imagesRelativePath", commonStuffRelativePath."images/");
+define ('commonStuffAbsolutePath', dirname(eiseIntraAbsolutePath).DIRECTORY_SEPARATOR);
 
-define ("eiseIntraCookiePath", "/"); // could be ("eiseIntraCookiePath", "/eiseAdmin/"); for eiseAdmin to allow its co-existance with other systems
-define ("eiseIntraCookieExpire", time()+60*60*24*30); // 30 days
+define ('jQueryPath', eiseIntraLibRelativePath.'jquery/');
+define ('jQueryUIPath', eiseIntraLibRelativePath.'jquery-ui/');
+define ('BootstrapPath', eiseIntraLibRelativePath.'bootstrap/dist/');
+define ('LessPHPPath', eiseIntraLibAbsolutePath.'less.php'.DIRECTORY_SEPARATOR);
+
+define ('jQueryUITheme','redmond');
+define ('imagesRelativePath', commonStuffRelativePath.'images/');
+
+if(eiseIntraCookiePath=='')
+	define ('eiseIntraCookiePath', '/');
+if(eiseIntraCookieExpire=='')
+	define ('eiseIntraCookieExpire', time()+60*60*24*30); // 30 days
 
 define ('eiseIntraUserMessageCookieName', 'UserMessage');
 
-// replace values below for LDAP authentication
-$ldap_server = "2.12.85.06";
-$ldap_domain = "e-ise.com";
-$ldap_dn = "DC=ru,DC=e-ise,DC=com";
-$ldap_anonymous_login = "anon"."@".$ldap_domain;
-$ldap_anonymous_password = "anonpass";
+$eiseIntraConf = array(
+	'localLanguage' => 'ru'
+	, 'localCountry' => 'RU'
+	, 'localCurrency' => 'RUB'
+	, 'strFrontEndSuffix' => '.min'
+);
 
-define("prgDT", "/([0-9]{1,2})[\.\-\/]([0-9]{1,2})[\.\-\/]([0-9]{4})/i");
-$prgDT = prgDT;
-define("prgReplaceTo","\\3-\\2-\\1");
+/*
+Constants for development environment
+*/
+$eiseIntraConf = array_merge($eiseIntraConf, array(
+	'strSubTitle' => 'DEVELOPMENT'
+	, 'strSubTitleLocal' => 'Версия разработчика'
+	, 'flagCollectKeys' => true
+	, 'flagBuildLess' => true
+	, 'strFrontEndSuffix' => ''
+	));
+/*
+/Constants for development environment
+*/
 
-$strSubTitle = "DEVELOPMENT";
-$strSubTitleLocal = "Версия для разработки";
-
-// update these values according your localizaion
-$localLanguage = "ru";
-$localCountry = "RUS";
-$localCurrency = "RUB";
 ?>
