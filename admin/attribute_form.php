ee8ab5ff1de6f1318f41f4290613cdc8efa54842
Ilya Eliseev <ie@e-ise.com>
2017-06-22 20:28:27 +0300

    Attribute form

--- admin/attribute_form.php
+++ admin/attribute_form.php
@@ -5,12 +5,17 @@ $atrID  = (isset($_POST['atrID']) ? $_POST['atrID'] : $_GET['atrID'] );
 $atrEntityID  = (isset($_POST['atrEntityID']) ? $_POST['atrEntityID'] : $_GET['atrEntityID'] );
 $DataAction  = (isset($_POST['DataAction']) ? $_POST['DataAction'] : $_GET['DataAction'] );
 
+$intra->requireComponent('grid');
+
+
+
 if($intra->arrUsrData['FlagWrite']){
 
 switch($DataAction){
     case 'update':
         
         $oSQL->q('START TRANSACTION');
+        $oSQL->startProfiling();
         
         if ($atrID=="") {
             $sqlIns = "INSERT INTO stbl_attribute (
@@ -78,6 +83,38 @@ switch($DataAction){
                 WHERE `atrID` = ".$oSQL->e($atrID)." AND `atrEntityID` = ".$oSQL->e($atrEntityID)."";
             $oSQL->q($sqlUpd);
         }
+
+        foreach ($_POST['inp_sat_updated'] as $ix => $updated) {
+            if(!$updated)
+                continue;
+
+            $sqlFields = "satFlagEditable = ".(int)($_POST['satFlagEditable'][$ix])."
+                , satFlagShowInForm = ".(int)($_POST['satFlagShowInForm'][$ix])."
+                , satFlagShowInList = ".(int)($_POST['satFlagShowInList'][$ix])."
+                , satFlagTrackOnArrival = ".(int)($_POST['satFlagTrackOnArrival'][$ix])."
+                , satEditBy = '$intra->usrID', satEditDate = NOW()
+                ";
+            $sqlUpd = "UPDATE stbl_status_attribute SET {$sqlFields} 
+                WHERE satStatusID=".$oSQL->e($_POST['staID'][$ix])."
+                    AND satEntityID = ".$oSQL->e($atrEntityID)."
+                    AND satAttributeID = ".$oSQL->e($atrID);
+            $oSQL->q($sqlUpd);        
+
+            if($oSQL->a()==0){
+                $sqlIns = "INSERT INTO stbl_status_attribute SET {$sqlFields}
+                    , satStatusID=".$oSQL->e($_POST['staID'][$ix])."
+                    , satEntityID = ".$oSQL->e($atrEntityID)."
+                    , satAttributeID = ".$oSQL->e($atrID)."
+                    , satInsertBy = '$intra->usrID', satInsertDate = NOW()";
+                $oSQL->q($sqlIns);
+            }
+        }
+/*
+        echo '<pre>';
+        print_r($_POST);
+        $oSQL->showProfileInfo();
+        die();
+        */
         
         $oSQL->q('COMMIT');
         
@@ -101,26 +138,26 @@ switch($DataAction){
 }
 }
 
-$sqlATR = "SELECT * FROM `stbl_attribute` WHERE `atrID` = ".$oSQL->e($atrID)." AND `atrEntityID` = ".$oSQL->e($atrEntityID)."";
+$sqlATR = "SELECT * FROM `stbl_attribute` 
+    INNER JOIN stbl_entity ON atrEntityID=entID
+    WHERE `atrID` = ".$oSQL->e($atrID)." AND `atrEntityID` = ".$oSQL->e($atrEntityID)."";
 $rsATR = $oSQL->do_query($sqlATR);
 $rwATR = $oSQL->fetch_array($rsATR);
 
-$arrActions[]= Array ('title' => 'Back to list'
+$arrActions[]= Array ('title' => $intra->translate('Back to %s', $rwATR['entTitle'])
 	   , 'action' => "entity_form.php?entID={$atrEntityID}&dbName={$dbName}"
 	   , 'class'=> 'ss_arrow_left'
 	);
 
-$intra->requireComponent('jquery-ui');
-
 include eiseIntraAbsolutePath.'inc_top.php';
 ?>
 
-<form action="<?php  echo $_SERVER["PHP_SELF"] ; ?>" method="POST" class="eiseIntraForm">
+<form action="<?php  echo $_SERVER["PHP_SELF"] ; ?>" method="POST" class="eiseIntraForm" id="frmAtr">
 <input type="hidden" name="atrID" value="<?php  echo htmlspecialchars($atrID) ; ?>">
 <input type="hidden" name="atrEntityID" value="<?php  echo htmlspecialchars($atrEntityID) ; ?>">
 <input type="hidden" name="DataAction" value="update">
 
-<fieldset class="eiseIntraMainForm"><legend>Attribute</legend>
+<fieldset class="eiseIntraMainForm" id="fldsMain"><legend>Attribute <?php echo ($rwATR["atrTitle"] ? '"'.$rwATR["atrTitle"].'"' : '')." ({$rwATR['atrID']})" ?> </legend>
 
 <div class="eiseIntraField">
 <label><?php echo $intra->translate("Title in English"); ?>:</label><?php
@@ -212,12 +249,125 @@ if ($atrID!="" && $rwATR["atrDeleteDate"]==""){
 </div>
 
 </fieldset>
+
+<?php 
+$gridSAT = new easyGrid($oSQL
+        ,'sat'
+        , Array(
+                'rowNum' =>40
+                , 'arrPermissions' => Array('FlagWrite'=>$intra->arrUsrData["FlagWrite"])
+                , 'strTable' => 'stbl_action_attribute'
+                , 'strPrefix' => 'aat'
+                , 'flagStandAlone' => true
+                , 'flagNoDelete' => true
+                )
+        );
+
+$gridSAT->Columns[]  = Array(
+            'type' => 'row_id'
+            , 'field' => 'staID'
+        );
+$gridSAT->Columns[] = Array(
+        'title' => ""
+        , 'field' => "satAttributeID"
+        , 'type' => "text"
+        , 'default' => $atrID
+);
+
+$gridSAT->Columns[] = Array(
+        'title' => ""
+        , 'field' => "satEntityID"
+        , 'type' => "text"
+        , 'default' => $atrEntityID
+);
+
+$gridSAT->Columns[] = Array(
+        'title' => $intra->translate("ID")
+        , 'field' => "staID_"
+        , 'type' => "number"
+        , 'disabled' => true
+        , 'width' => "30px"
+);
+$gridSAT->Columns[] = Array(
+        'title' => $intra->translate("Title")
+        , 'field' => "staTitle{$intra->local}"
+        , 'type' => "text"
+        , 'disabled' => true
+        , 'width' => "100%"
+);
+
+$gridSAT->Columns[] = Array(
+        'title' => $intra->translate("Track?")
+        , 'field' => "satFlagTrackOnArrival"
+        , 'type' => "checkbox"
+        , 'width' => '40px'
+);
+
+$gridSAT->Columns[] = Array(
+        'title' => $intra->translate("In Form?")
+        , 'field' => "satFlagShowInForm"
+        , 'type' => "checkbox"
+        , 'width' => '40px'
+);
+$gridSAT->Columns[] = Array(
+        'title' => $intra->translate("Allowed?")
+        , 'field' => "satFlagEditable"
+        , 'type' => "checkbox"
+        , 'width' => '40px'
+);
+$gridSAT->Columns[] = Array(
+        'title' => $intra->translate("In List?")
+        , 'field' => "satFlagShowInList"
+        , 'type' => "checkbox"
+        , 'width' => '40px'
+);
+
+
+$sqlSAT = "SELECT * FROM stbl_status 
+    LEFT OUTER JOIN stbl_status_attribute ON staID=satStatusID AND satAttributeID='$atrID' AND satEntityID='$atrEntityID'
+    WHERE staEntityID='$atrEntityID'
+    ORDER BY staID";
+$rsSAT = $oSQL->do_query($sqlSAT);
+while ($rwSAT = $oSQL->fetch_array($rsSAT)){
+    $rwSAT["staID_"] = $rwSAT["staID"];
+    $rwSAT["satAllowed"] = ($rwSAT["satID"] ? "1" : "0");
+    $gridSAT->Rows[] = $rwSAT;
+}
+
+echo $intra->fieldset($intra->translate('Status visibility'), $gridSAT->get_html(), array('id'=>'fldsAttr'));
+
+
+ ?>
+
 </form>
 <script>
 $(document).ready(function(){
     eiseIntraInitializeForm();
+    $('.eiseGrid').eiseGrid();
+    $("th.sat-satFlagShowInForm, th.sat-satFlagEditable, th.sat-satFlagShowInList")
+        .css("cursor", "pointer")
+        .click(function(){
+            var tdClass = ($(this).attr('class').split(/\s+/)[0]),
+                $grid = $(this).parents('.eiseGrid').first(),
+                $checkBoxes = $grid.find('.eg-data .'+tdClass+' input[type=checkbox]');
+            $checkBoxes.each(function(){
+                this.click();
+            })
+        });
 });
 </script>
+<style type="text/css">
+#frmAtr fieldset {
+    display: inline-block;
+    vertical-align: top;
+}
+#fldsMain {
+    width: 62%;
+}
+#fldsAttr {
+    width: 36%;
+}
+</style>
 <?php
 include eiseIntraAbsolutePath.'inc_bottom.php';
 ?>
