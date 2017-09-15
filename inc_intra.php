ea77d2ffe3fd43803f65a5e98f0a0efc89899014
Ilya Eliseev <ie@e-ise.com>
2017-09-15 19:23:25 +0300

    version update

--- inc_intra.php
+++ inc_intra.php
@@ -18,7 +18,7 @@ include "inc_item.php";
 
 class eiseIntra extends eiseIntraData {
 
-public $conf = array('versionIntra'=>'2.0beta.040');
+public $conf = array('versionIntra'=>'2.0beta.041');
 
 private $arrHTML5AllowedInputTypes = 
     Array("color"
