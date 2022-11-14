1d0a989a156679980d0ed44f10497a8d5753ecaf
Ilya Eliseev <ie@e-ise.com>
2022-11-14 15:24:10 +0300

    sendMessages flood fix

--- inc_item.php
+++ inc_item.php
@@ -793,11 +793,9 @@ static function sendMessages($conf){
 
     $oSQL = $intra->oSQL;
 
-    $oSQL->q('START TRANSACTION');
     $oSQL->q("UPDATE stbl_message SET msgSendDate=NOW()
             , msgStatus='Sending' 
             WHERE msgSendDate IS NULL");
-   	$oSQL->q('COMMIT');
 
     // scan table for unsent messages
     $sqlMsg = "SELECT * FROM stbl_message 
@@ -892,7 +890,12 @@ static function sendMessages($conf){
 
 
     foreach((array)$arrMessages as $msg){
-        $sqlMarkSent = "UPDATE stbl_message SET msgSendDate=".($msg['send_time'] ? "'".date('Y-m-d H:i:s', $msg['send_time'])."'" : 'NULL' )."
+        $sqlMarkSent = "UPDATE stbl_message SET msgSendDate=".($msg['error'] 
+                ? 'NULL' 
+                : ($msg['send_time'] 
+                    ? "'".date('Y-m-d H:i:s', $msg['send_time'])."'" 
+                    : 'NOW()' )
+                )."
             , msgStatus=".($msg['error'] ? $oSQL->e($msg['error']) : $oSQL->e('Sent'))."
             , msgPassword=NULL
             , msgEditDate=NOW()
