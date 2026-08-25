fa43e0a60e2125a384dbd01714bebb3520ef0091
Ilya Eliseev <ie@e-ise.com>
2026-08-25 15:04:04 +0300

    inc_top aring theme fix

--- inc_top.php
+++ inc_top.php
@@ -35,7 +35,11 @@ $conf_to_set = array_merge($intra->conf, [ 'FlagWrite'=>(int)$intra->arrUsrData[
 
 <?php if (!$flagNoHeader): ?>
     <div id="header" class="ei-header" role="nav">
+        <?php if ($eiseIntraCSSTheme!='aring'): ?>
+        <div class="ei-app-title"><span class="sidebar-toggle fa fa-bars"></span><a href="<?php echo dirname($_SERVER['PHP_SELF']).'/' ?>"><?php echo $title; ?></a></div>
+        <?php else: ?>
         <div class="ei-app-title"><span class="sidebar-toggle fa fa-bars"></span><a href="<?php echo dirname($_SERVER['PHP_SELF']).'/' ?>"><?php echo preg_replace('/([aAåÅаА])/u', '<span class="ei-title-logo">$1</span>', htmlspecialchars($title), 1); ?></a></div>
+        <?php endif ?>
         <div class="ei-top-level-menu-container"> </div>
         
         <?php if ($warning): ?>
